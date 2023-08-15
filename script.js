0ddf2a0f4e4dd3b97a89c435df733ad941023e09
CristiSorlei <141819093+CristiSorlei@users.noreply.github.com>
2023-08-15 23:32:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,82 @@
-const noteList = document.getElementById('noteList');
-const notes = JSON.parse(localStorage.getItem('notes')) || [];
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
 
 function addNote() {
   const noteText = document.getElementById('noteText').value;
   const noteColor = document.getElementById('noteColor').value;
-
-  if (noteText.trim() === '') {
-    alert('Introduceți un text pentru notiță!');
-    return;
-  }
-
-  const newNote = {
-    text: noteText,
-    color: noteColor
-  };
-
-  notes.push(newNote);
-  saveNotes();
-  updateNoteList();
-
-  // Resetarea formularului
-  document.getElementById('noteText').value = '';
-}
-
-function saveNotes() {
+  const note = { text: noteText, color: noteColor };
+  notes.push(note);
   localStorage.setItem('notes', JSON.stringify(notes));
+  saveNoteToLocalStorage(note);
+  renderNotes();
 }
 
-function updateNoteList() {
+function saveNoteToLocalStorage(note) {
+  const allNotes = JSON.parse(localStorage.getItem('allNotes')) || [];
+  
+  // Adăugați spații între cuvinte înainte de a salva nota în local storage
+  const formattedNote = { text: note.text.replace(/\n/g, ' '), color: note.color };
+  
+  allNotes.push(formattedNote);
+  localStorage.setItem('allNotes', JSON.stringify(allNotes));
+}
+
+function deleteNote(index) {
+  notes.splice(index, 1);
+  localStorage.setItem('notes', JSON.stringify(notes));
+  deleteNoteFromLocalStorage(index); // Adăugați această linie pentru a șterge nota individual din localStorage
+  renderNotes();
+}
+
+function deleteNoteFromLocalStorage(index) {
+  const allNotes = JSON.parse(localStorage.getItem('allNotes'));
+  allNotes.splice(index, 1);
+  localStorage.setItem('allNotes', JSON.stringify(allNotes));
+}
+
+function renderNotes() {
+  const noteList = document.getElementById('noteList');
   noteList.innerHTML = '';
-  notes.forEach(function (note) {
-    const noteElement = createNoteElement(note.text, note.color);
+  
+  const allNotes = JSON.parse(localStorage.getItem('allNotes')) || [];
+  
+  for (let i = 0; i < allNotes.length; i++) {
+    const note = allNotes[i];
+    const noteElement = document.createElement('div');
+    noteElement.classList.add('note');
+    noteElement.style.backgroundColor = note.color;
+    const noteTextElement = document.createElement('div');
+    noteTextElement.contentEditable = true;
+    noteTextElement.textContent = note.text;
+    noteTextElement.addEventListener('input', () => {
+      note.text = noteTextElement.textContent;
+      updateNoteInLocalStorage(i, note);
+    });
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Sterge';
+    deleteButton.addEventListener('click', () => {
+      deleteNote(i);
+    });
+    noteElement.appendChild(noteTextElement);
+    noteElement.appendChild(deleteButton);
     noteList.appendChild(noteElement);
-  });
-}
-
-function createNoteElement(text, color) {
-  const noteContainer = document.createElement('div');
-  noteContainer.classList.add('note-container');
-
-  const noteElement = document.createElement('div');
-  noteElement.classList.add('note', color);
-  noteElement.setAttribute('contenteditable', 'true');
-
-  const noteTextElement = document.createElement('span');
-  noteTextElement.textContent = text;
-  noteTextElement.classList.add('note-text');
-
-  noteElement.appendChild(noteTextElement);
-
-  noteTextElement.addEventListener('mouseenter', function () {
-    if (noteTextElement.offsetWidth < noteTextElement.scrollWidth) {
-      noteTextElement.title = text;
-    }
-  });
-
-  noteTextElement.addEventListener('mouseleave', function () {
-    noteTextElement.removeAttribute('title');
-  });
-
-  noteElement.addEventListener('input', function () {
-    updateNoteText(noteElement);
-  });
-
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'Șterge';
-  deleteButton.addEventListener('click', function () {
-    deleteNoteContainer(noteContainer);
-  });
-
-  noteContainer.appendChild(noteElement);
-  noteContainer.appendChild(deleteButton);
-
-  return noteContainer;
-}
-
-function updateNoteText(noteElement) {
-  const index = Array.from(noteList.children).indexOf(noteElement);
-  if (index !== -1) {
-    notes[index].text = noteElement.textContent;
-    saveNotes();
   }
 }
 
-function deleteNoteContainer(noteContainer) {
-  const index = Array.from(noteList.children).indexOf(noteContainer);
-  if (index !== -1) {
-    notes.splice(index, 1);
-    saveNotes();
-    updateNoteList();
-  }
+renderNotes();
+
+function updateNoteInLocalStorage(index, note) {
+  const allNotes = JSON.parse(localStorage.getItem('allNotes'));
+  allNotes[index] = note;
+  localStorage.setItem('allNotes', JSON.stringify(allNotes));
+  noteTextElement.addEventListener('input', () => {
+    note.text = noteTextElement.textContent;
+    updateNoteInLocalStorage(i, note);
+  });
 }
 
-updateNoteList();
-
-document.getElementById('noteText').addEventListener('keyup', function(event) {
-  if (event.key === 'Enter') {
-    addNote();
-  }
+const noteTextElement = document.createElement('textarea');
+noteTextElement.value = note.text;
+noteTextElement.addEventListener('input', () => {
+  note.text = noteTextElement.value;
+  updateNoteInLocalStorage(i, note);
 });
